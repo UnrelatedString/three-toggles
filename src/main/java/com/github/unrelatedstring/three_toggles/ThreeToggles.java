c05e0e9e313a3f06f578ae828df5ce169f801b7f
Kai Hoop <unrelatedstring@gmail.com>
2020-07-26 09:29:07 -0400

    It's worth putting it out there that this alone does NOT work

--- src/main/java/com/github/unrelatedstring/three_toggles/ThreeToggles.java
+++ src/main/java/com/github/unrelatedstring/three_toggles/ThreeToggles.java
@@ -2,8 +2,10 @@ package com.github.unrelatedstring.three_toggles;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.options.StickyKeyBinding;
 import org.lwjgl.glfw.GLFW;
 
 public class ThreeToggles implements ClientModInitializer {
@@ -11,27 +13,35 @@ public class ThreeToggles implements ClientModInitializer {
 	public static KeyBinding toggleWalk;
 	public static KeyBinding toggleMine;
 
+	private static void onTick(MinecraftClient client) {
+		if (toggleSneak.isPressed()) {
+			client.player.setSneaking(true);
+		}
+	}
+
 	@Override
 	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		toggleSneak = new KeyBinding("key.three_toggles.sneak",
-				   			         InputUtil.Type.KEYSYM,
-									 GLFW.GLFW_KEY_V,
-									 "category.three_toggles.toggles");
-		toggleWalk =  new KeyBinding("key.three_toggles.walk",
-									 InputUtil.Type.KEYSYM,
-				                     GLFW.GLFW_KEY_B,
-					                 "category.three_toggles.toggles");
-		toggleMine =  new KeyBinding("key.three_toggles.mine",
-								     InputUtil.Type.KEYSYM,
-								     GLFW.GLFW_KEY_N,
-					        		 "category.three_toggles.toggles");
+		toggleSneak = new StickyKeyBinding("key.three_toggles.sneak",
+										   GLFW.GLFW_KEY_V,
+				             	           "category.three_toggles.toggles",
+		                                   () -> true);
+		toggleWalk =  new StickyKeyBinding("key.three_toggles.walk",
+									       GLFW.GLFW_KEY_B,
+					                       "category.three_toggles.toggles",
+							  	           () -> true);
+		toggleMine =  new StickyKeyBinding("key.three_toggles.mine",
+								     	   GLFW.GLFW_KEY_N,
+					        		 	   "category.three_toggles.toggles",
+										   () -> true);
 
 		KeyBindingHelper.registerKeyBinding(toggleSneak);
 		KeyBindingHelper.registerKeyBinding(toggleWalk);
 		KeyBindingHelper.registerKeyBinding(toggleMine);
+
+		ClientTickCallback.EVENT.register(ThreeToggles::onTick);
 	}
 }

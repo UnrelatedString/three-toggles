0358c4da712a02f710acec7e74a5ebde8360db0c
Kai Hoop <unrelatedstring@gmail.com>
2020-07-27 05:05:36 -0400

    Refactored; added all three toggles

--- src/main/java/com/github/unrelatedstring/three_toggles/ThreeToggles.java
+++ src/main/java/com/github/unrelatedstring/three_toggles/ThreeToggles.java
@@ -1,25 +1,12 @@
 package com.github.unrelatedstring.three_toggles;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.event.client.ClientTickCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.options.StickyKeyBinding;
-import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class ThreeToggles implements ClientModInitializer {
-	public static KeyBinding toggleSneak;
-	public static KeyBinding toggleWalk;
-	public static KeyBinding toggleMine;
-
-	private static void onTick(MinecraftClient client) {
-		if (toggleSneak.isPressed()) {
-			client.options.keySneak.setPressed(true);
-			System.out.println(client.player.isSneaking());
-		}
-	}
+	public static ToggleBinding toggleSneak;
+	public static ToggleBinding toggleWalk;
+	public static ToggleBinding toggleMine;
 
 	@Override
 	public void onInitializeClient() {
@@ -27,23 +14,15 @@ public class ThreeToggles implements ClientModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		toggleSneak = new StickyKeyBinding("key.three_toggles.sneak",
-										   GLFW.GLFW_KEY_V,
-				             	           "category.three_toggles.toggles",
-		                                   () -> true);
-		toggleWalk  = new StickyKeyBinding("key.three_toggles.walk",
-									       GLFW.GLFW_KEY_B,
-					                       "category.three_toggles.toggles",
-							  	           () -> true);
-		toggleMine  = new StickyKeyBinding("key.three_toggles.mine",
-								     	   GLFW.GLFW_KEY_N,
-					        		 	   "category.three_toggles.toggles",
-										   () -> true);
+		toggleSneak = new ToggleBinding("key.three_toggles.sneak",
+									    GLFW.GLFW_KEY_V,
+						  			    (client) -> client.options.keySneak);
+		toggleWalk  = new ToggleBinding("key.three_toggles.walk",
+				    				    GLFW.GLFW_KEY_B,
+									    (client) -> client.options.keyForward);
+		toggleMine  = new ToggleBinding("key.three_toggles.mine",
+										GLFW.GLFW_KEY_N,
+										(client) -> client.options.keyAttack);
 
-		KeyBindingHelper.registerKeyBinding(toggleSneak);
-		KeyBindingHelper.registerKeyBinding(toggleWalk);
-		KeyBindingHelper.registerKeyBinding(toggleMine);
-
-		ClientTickCallback.EVENT.register(ThreeToggles::onTick);
 	}
 }
---
+++ src/main/java/com/github/unrelatedstring/three_toggles/ToggleBinding.java
@@ -0,0 +1,31 @@
+package com.github.unrelatedstring.three_toggles;
+
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.KeyBinding;
+
+import java.util.function.Function;
+
+public class ToggleBinding {
+    public KeyBinding binding;
+    public boolean on = false;
+    public Function<MinecraftClient, KeyBinding> target;
+
+    public ToggleBinding(String id, int key, Function<MinecraftClient, KeyBinding> target) {
+        this.target = target;
+        binding = new KeyBinding(id, key, "category.three_toggles.toggles");
+        KeyBindingHelper.registerKeyBinding(binding);
+        ClientTickCallback.EVENT.register(this::tick);
+    }
+
+    public void tick(MinecraftClient client) {
+        if (binding.wasPressed()) {
+            while (binding.wasPressed()) ;
+            on = !on;
+        }
+        if (on) {
+            target.apply(client).setPressed(true);
+        }
+    }
+}

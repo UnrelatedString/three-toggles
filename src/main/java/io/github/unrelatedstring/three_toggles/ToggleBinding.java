ec7f5f1c1309bb12382edc2a68d4fb7699a0c723
Kai Hoop <unrelatedstring@gmail.com>
2020-07-28 05:48:36 -0400

    I am shocked
    This works fully as intended. I was SURE I'd need *something* to make sure the toggle on and toggle off worked precisely as intended, but... I forgot which method I was throwing a mixin at. So I'm actually not surprised at all

--- src/main/java/io/github/unrelatedstring/three_toggles/ToggleBinding.java
+++ src/main/java/io/github/unrelatedstring/three_toggles/ToggleBinding.java
@@ -21,31 +21,21 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.options.StickyKeyBinding;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 public class ToggleBinding {
     public KeyBinding binding;
-    public boolean on = false;
     public Function<MinecraftClient, KeyBinding> target;
+    public static Map<String, ToggleBinding> toggles = new HashMap<>();
 
-    public ToggleBinding(String id, int key, Function<MinecraftClient, KeyBinding> target) {
+    public ToggleBinding(String id, int key, Function<MinecraftClient, KeyBinding> target, String targetTranslationKey) {
         this.target = target;
-        binding = new KeyBinding(id, key, "category.three_toggles.toggles");
+        binding = new StickyKeyBinding(id, key, "category.three_toggles.toggles", () -> true);
         KeyBindingHelper.registerKeyBinding(binding);
-        ClientTickCallback.EVENT.register(this::tick);
-    }
-
-    public void tick(MinecraftClient client) {
-        if (binding.wasPressed()) {
-            while (binding.wasPressed()) {}
-            if (on) {
-                target.apply(client).setPressed(false);
-            }
-            on = !on;
-        }
-        if (on) {
-            target.apply(client).setPressed(true);
-        }
+        toggles.put(targetTranslationKey, this);
     }
 }

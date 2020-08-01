636dfe9519c63d3e696bea50de866402cf858b82
Kai Hoop <unrelatedstring@gmail.com>
2020-08-01 05:25:10 -0400

    It sucks, but bindings are now taken from the config
    I say it sucks because it assigns default keys in the wrong order and doesn't reload the bindings until you restart the client

--- src/main/java/io/github/unrelatedstring/three_toggles/ThreeToggles.java
+++ src/main/java/io/github/unrelatedstring/three_toggles/ThreeToggles.java
@@ -6,31 +6,59 @@
 
 package io.github.unrelatedstring.three_toggles;
 
+import com.sun.tools.internal.xjc.model.CDefaultValue;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import org.lwjgl.glfw.GLFW;
 
 public class ThreeToggles implements ClientModInitializer {
-    public static ToggleBinding toggleSneak;
-    public static ToggleBinding toggleWalk;
-    public static ToggleBinding toggleMine;
+
+    // designed for QWERTY, respecting default vanilla key bindings
+    static final int[] defaultKeys = {
+            GLFW.GLFW_KEY_V,
+            GLFW.GLFW_KEY_B,
+            GLFW.GLFW_KEY_N,
+            GLFW.GLFW_KEY_M,
+            GLFW.GLFW_KEY_COMMA,
+            GLFW.GLFW_KEY_PERIOD,
+            GLFW.GLFW_KEY_G,
+            GLFW.GLFW_KEY_H,
+            GLFW.GLFW_KEY_J,
+            GLFW.GLFW_KEY_K,
+            GLFW.GLFW_KEY_L,
+            GLFW.GLFW_KEY_SEMICOLON,
+            GLFW.GLFW_KEY_APOSTROPHE,
+            GLFW.GLFW_KEY_R,
+            GLFW.GLFW_KEY_Y,
+            GLFW.GLFW_KEY_U,
+            GLFW.GLFW_KEY_I,
+            GLFW.GLFW_KEY_O,
+            GLFW.GLFW_KEY_P,
+            GLFW.GLFW_KEY_LEFT_BRACKET,
+            GLFW.GLFW_KEY_RIGHT_BRACKET,
+            GLFW.GLFW_KEY_BACKSLASH,
+            GLFW.GLFW_KEY_MINUS,
+            GLFW.GLFW_KEY_EQUAL,
+            GLFW.GLFW_KEY_BACKSPACE
+    };
 
     @Override
     public void onInitializeClient() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-
-        toggleSneak = new ToggleBinding(GLFW.GLFW_KEY_V,
-                "key.sneak");
-        toggleWalk = new ToggleBinding(GLFW.GLFW_KEY_B,
-                "key.forward");
-        toggleMine = new ToggleBinding(GLFW.GLFW_KEY_N,
-                "key.attack");
 
         AutoConfig.register(ToggleConfig.class, GsonConfigSerializer::new);
-        ToggleConfig config = AutoConfig.getConfigHolder(ToggleConfig.class).getConfig();
+        setBindings();
 
+    }
+
+    public void setBindings() {
+        ToggleBinding.toggles.clear();
+        ToggleConfig config = AutoConfig.getConfigHolder(ToggleConfig.class).getConfig();
+        int i = 0;
+        for (String target : config.targets) {
+            new ToggleBinding(
+                    i < defaultKeys.length ? defaultKeys[i++] : -1,
+                    target);
+        }
     }
 }
